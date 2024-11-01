47bab38a234d3d9bbd1281ded21cdebbdc0f6dc3
Vector <jianjian125125@gmail.com>
2024-11-01 20:26:36 +0800

    update dependencies

--- lingle.py
+++ lingle.py
@@ -1,101 +1,84 @@
-# Imports the Google Cloud client library
-from google.cloud import language
-from google.cloud.language import enums
-from google.cloud.language import types
+ # Imports the Google Cloud client library
+from google.cloud import language_v1
 from split_article import find_sentences
 
 # Instantiates a client
-client = language.LanguageServiceClient()
+client = language_v1.LanguageServiceClient()
 
 # The text to analyze
-text = open("article.txt", 'r', encoding="utf8")
-text_to_read = text.read()
-document = types.Document(content=text_to_read,
-                          type=enums.Document.Type.PLAIN_TEXT)
+with open("/Users/vector/Documents/GitHub/uofthacks6/article.txt", 'r', encoding="utf8") as text:
+    text_to_read = text.read()
+
+# Create a document for analysis
+document = language_v1.Document(content=text_to_read, type_=language_v1.Document.Type.PLAIN_TEXT)
 
 # Detects the sentiment of the text
 sentiment = client.analyze_sentiment(document=document).document_sentiment
-# Detects the entities of the text
-response = client.analyze_entities(document=document, encoding_type='UTF32')
-# Detects the entities' score of the text
-response_two = client.analyze_entity_sentiment(document=document,
-                                               encoding_type='UTF32')
 
-# build four list containing all the information from the text
-# like the name, type,metadata and salience
-i = 0
+# Detects the entities and their sentiment within the text
+response = client.analyze_entities(document=document, encoding_type=language_v1.EncodingType.UTF32)
+response_two = client.analyze_entity_sentiment(document=document, encoding_type=language_v1.EncodingType.UTF32)
+
+# Build lists for storing entity details
 text_name = []
 text_type = []
 text_metadata = []
 text_salience = []
 text_score = []
 
-for entity in response.entities:
+# Ensure we only iterate up to the shorter of response entities
+min_length = min(len(response.entities), len(response_two.entities))
+
+for i in range(min_length):
+    entity = response.entities[i]
     text_name.append(entity.name)
-    text_type.append(entity.type)
+    text_type.append(entity.type_)
     text_metadata.append(entity.metadata)
     text_salience.append(entity.salience)
     text_score.append(response_two.entities[i].sentiment.score)
-    i = i + 1
 
-# list these list in order according to the salience
-length = len(text_salience)
-for j in range(0, length - 1):
-    for k in range(0, length - j - 1):
-        if text_salience[k] < text_salience[k + 1]:
-            text_name[k], text_name[k + 1] = text_name[k + 1], text_name[k]
-            text_type[k], text_type[k + 1] = text_type[k + 1], text_type[k]
-            text_metadata[k], text_metadata[k + 1] = text_metadata[k + 1], \
-                text_metadata[k]
-            text_salience[k], text_salience[k + 1] = text_salience[k + 1], \
-                text_salience[k]
-            text_score[k], text_score[k + 1] = text_score[k + 1], text_score[k]
+# Sort entities by salience in descending order
+sorted_indices = sorted(range(len(text_salience)), key=lambda k: text_salience[k], reverse=True)
+text_name = [text_name[i] for i in sorted_indices]
+text_type = [text_type[i] for i in sorted_indices]
+text_metadata = [text_metadata[i] for i in sorted_indices]
+text_salience = [text_salience[i] for i in sorted_indices]
+text_score = [text_score[i] for i in sorted_indices]
 
-# remove the entities with type == OTHER from text_name
-for entity in response.entities:
-    entity_type = enums.Entity.Type(entity.type)
-    if entity_type.name == "OTHER":
-        index = text_name.index(entity.name)
-        text_name.remove(entity.name)
-        del text_score[index]
+# Remove entities with type == OTHER
+filtered_entities = [(name, score) for name, type_, score in zip(text_name, text_type, text_score)
+                     if type_ != language_v1.Entity.Type.OTHER]
+text_name, text_score = zip(*filtered_entities) if filtered_entities else ([], [])
 
-# take the first 3 entities with the highest salience,
-# if the len(text_name) >= 3
-# otherwise, take all entities in text_name
-i = min(3, len(text_name))
-first_three_entities = text_name[:i]
-first_three_entities_score = text_score[:i]
+# Get the top 3 entities based on salience
+first_three_entities = text_name[:3]
+first_three_entities_score = text_score[:3]
 
-# add the sentence containing one of the three entities into list_extract
+# Extract sentences containing the top entities
 sentence_list = find_sentences(text_to_read)
-blue = []
-red = []
-yellow = []
-for sentence in sentence_list:
-    whether_in = 0
-    score = 0
-    if first_three_entities[0] in sentence:
-        whether_in = 1
-    elif first_three_entities[1] in sentence:
-        whether_in = 1
-    elif first_three_entities[2] in sentence:
-        whether_in = 1
-    if whether_in == 1:
-        if first_three_entities[0] in sentence:
-            score += first_three_entities_score[0]
-        elif first_three_entities[1] in sentence:
-            score += first_three_entities_score[1]
-        elif first_three_entities[2] in sentence:
-            score += first_three_entities_score[2]
-    if (score == 0 and whether_in == 1) is True:
-        yellow.append(sentence)
-    elif score < 0:
-        blue.append(sentence)
-    elif score > 0:
-        red.append(sentence)
+yellow = []  # Sentences with neutral sentiment (0)
+blue = []    # Sentences with negative sentiment (< 0)
+red = []     # Sentences with positive sentiment (> 0)
 
-print(yellow)
-print("\n\n\n")
-print(blue)
-print("\n\n\n")
-print(red)
+for sentence in sentence_list:
+    score = 0
+    in_entity = False
+    for entity, entity_score in zip(first_three_entities, first_three_entities_score):
+        if entity in sentence:
+            in_entity = True
+            score += entity_score
+            break  # Stop after finding the first relevant entity in the sentence
+
+    if in_entity:
+        if score == 0:
+            yellow.append(sentence)
+        elif score < 0:
+            blue.append(sentence)
+        else:
+            red.append(sentence)
+
+# Output categorized sentences
+print("Neutral Sentences (Yellow):", yellow)
+print("\nNegative Sentences (Blue):", blue)
+print("\nPositive Sentences (Red):", red)
+
